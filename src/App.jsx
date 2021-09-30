04630a7c9deba0cef27d6246669daa312844845b
Yuri Nalin <yuri.nalin.rms@gmail.com>
2021-09-30 18:00:56 +0200

    Added Bootstrap and visual adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,18 @@ function App() {
 
   return (
     <div className="App">
-      <div className="app-input">
-        <input type="file" name="fileInput" id="fileInput" onChange={handleFileSelection} />
+      <div className="app-input pt-3">
+        <input className="form-control custom-file-input" type="file" name="fileInput" id="fileInput" onChange={handleFileSelection} />
       </div>
-      <div className="app-output">
-        {`Current file selected: ${file}`}
+      <div className="app-output pt-3">
+        <div className="form-switch switch-box">
+          <input className="form-check-input custom-check-input" type="checkbox" disabled id="flexSwitchCheckDefault" />
+          <label className="form-check-label check-label-left" for="flexSwitchCheckDefault">Texto</label>
+          <label className="form-check-label check-label-right" for="flexSwitchCheckDefault">Imagem</label>
+        </div>
+        <div className="output-display">
+          {`Current file selected: ${file}`}
+        </div>
       </div>
     </div>
   );
