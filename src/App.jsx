0284fc40d10c2f3a78c8580849b306532303ed37
Yuri Nalin <yuri.nalin.rms@gmail.com>
2021-09-30 19:14:11 +0200

    Added file reading logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+const reader = new FileReader();
 
 function App() {
   const [file, setFile] = useState(null);
 
+  /**
+   * Handles file selection for input element.
+   * @param {*} e onChange event object
+   */
   const handleFileSelection = (e) => {
-    const fileName = e.target?.value;
-    setFile(fileName);
+    const file = e.target?.files?.[0];
+    // console.log(file);
+
+    try {
+      reader.readAsText(file);
+    } catch (error) {
+      setFile(null);
+    }
   }
+
+  /**
+   * Sets file content to state.
+   * @param {*} e onLoad event object
+   */
+  const handleLoadFile = (e) => {
+    setFile(e.target?.result);
+  }
+
+  useEffect(() => {
+    // Add eventListener to FileReader when component mounts
+    reader.addEventListener('load', handleLoadFile);
+
+    // Cleanup eventlistener when component unmounts
+    return () => { reader.removeEventListener('load', handleLoadFile) }
+  });
 
   return (
     <div className="App">
       <div className="app-input pt-3">
-        <input className="form-control custom-file-input" type="file" name="fileInput" id="fileInput" onChange={handleFileSelection} />
+        <input className="form-control custom-file-input" type="file" name="fileInput" id="fileInput" accept=".txt" onChange={handleFileSelection} />
       </div>
       <div className="app-output pt-3">
         <div className="form-switch switch-box">
@@ -21,7 +49,9 @@ function App() {
           <label className="form-check-label check-label-right" htmlFor="flexSwitchCheckDefault">Image</label>
         </div>
         <div className="output-display">
-          {`Current file selected: ${file}`}
+          <p>
+            {`${file || ""}`}
+          </p>
         </div>
       </div>
     </div>
